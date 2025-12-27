cdb6ac45f95c34f43df3570913526e7cfbde27cb
muozez <muhammetylmaz128@gmail.com>
2025-12-27 16:38:34 +0300

    handle quit

--- net-server/client.js
+++ net-server/client.js
@@ -18,16 +18,23 @@ rl.prompt();
 
 socket.on('connect', ()=>{
     rl.on('line', line => {
+        if (line === 'quit'){
+            socket.end()
+            rl.close();
+            return
+        }
         socket.write(line + '\r\n')
         rl.prompt();
     });
 })
 
 socket.on('data', (data)=>{
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
     console.log(data.toString('utf-8'));
+    rl.prompt()
 });
 
 socket.on('close', ()=>{
     console.log("Disconnected")
 })
-
